21f1faab16f09ce6a23aa6c337f979c3b1e57376
Diego Medina Quilcat <diegoa_mq@hotmail.com>
2021-02-06 16:48:52 -0500

    :tada: Initial commit

---
+++ index.php
@@ -0,0 +1,228 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="generator" content="Hugo 0.79.0">
+    <title>Municipalidad de San Benito</title>
+
+    <!-- Bootstrap core CSS -->
+<link href="css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
+<link href="css/estilos.css" rel="stylesheet" crossorigin="anonymous">
+<link href="css/estilos-footer.css" rel="stylesheet" crossorigin="anonymous">
+<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
+
+    
+    <!-- Custom styles for this template -->
+    <link href="css/carousel.css" rel="stylesheet">
+  </head>
+  <body>
+   <!--fin NavBar -->   
+<header>
+  <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">MDSB</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarCollapse">
+        <ul class="navbar-nav me-auto mb-2 mb-md-0">
+          <li class="nav-item">
+            <a class="nav-link" href="index.php">Inicio</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Nosotros
+            </a>
+            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <li><a class="dropdown-item" href="#">Alcalde</a></li>
+              <li><a class="dropdown-item" href="#">Regidores</a></li>
+            </ul>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Oragnizacion
+            </a>
+            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <li><a class="dropdown-item" href="#">Misión</a></li>
+              <li><a class="dropdown-item" href="#">Visión</a></li>
+            </ul>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Contactenos</a>
+          </li>
+        </ul>
+        <form class="d-flex">
+          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Buscar">
+          <button class="btn btn-outline-success" type="submit">Buscar</button>
+        </form>
+      </div>
+    </div>
+  </nav>
+</header>
+  <!--fin Navbar -->
+
+  <!-- Carrusel -->
+<main>
+
+  <div id="myCarousel" class="carousel slide" data-bs-ride="carousel">
+    <ol class="carousel-indicators">
+      <li data-bs-target="#myCarousel" data-bs-slide-to="0" class="active"></li>
+      <li data-bs-target="#myCarousel" data-bs-slide-to="1"></li>
+      <li data-bs-target="#myCarousel" data-bs-slide-to="2"></li>
+    </ol>
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <img class="bd-placeholder-img" src="img/8.jpg" alt="" width="100%" height="100%">
+        <div class="container">
+          <div class="carousel-caption text-start bg-dark" style="background-color:transparent; opacity:.6;">
+            <h1>Plaza de Armas San Benito</h1>
+            <p>La Municipalidad Distrital de San Benito te da la Bienvenida</p>
+          </div>
+        </div>
+      </div>
+      <div class="carousel-item">
+        <img class="bd-placeholder-img" src="img/1.jpg" alt="" width="100%" height="100%">
+        <div class="container">
+          <div class="carousel-caption text-start bg-dark" style="background-color:transparent; opacity:.6;">
+            <h1>Plaza de Armas San Benito</h1>
+            <p>La Municipalidad Distrital de San Benito te da la Bienvenida</p>
+          </div>
+        </div>
+      </div>
+      <div class="carousel-item">
+        
+        <img class="bd-placeholder-img" src="img/3.jpg" alt="" width="100%" height="100%">
+        <div class="container">
+          <div class="carousel-caption text-start bg-dark" style="background-color:transparent; opacity:.6;">
+            <h1>Plaza de Armas San Benito</h1>
+            <p>La Municipalidad Distrital de San Benito te da la Bienvenida</p>
+           </div>
+      </div>
+    </div>
+    <a class="carousel-control-prev" href="#myCarousel" role="button" data-bs-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      
+      <span class="visually-hidden">Previous</span>
+    </a>
+    <a class="carousel-control-next" href="#myCarousel" role="button" data-bs-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Next</span>
+    </a>
+  </div>
+</div>
+
+ 
+  <!--fin Carrusel -->
+    <!--imagenes -->
+  <div class="container marketing">
+
+  
+    <div class="row">
+      <div class="col-lg-4">
+        <img class="bd-placeholder-img rounded-circle" src="img/1.jpg" alt="" width="140" height="140">
+       
+        <h2>Heading</h2>
+        <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
+        <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
+      </div><!-- /.col-lg-4 -->
+      <div class="col-lg-4">
+        <img class="bd-placeholder-img rounded-circle" src="img/1.jpg" alt="" width="140" height="140">
+       
+        <h2>Heading</h2>
+        <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.</p>
+        <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
+      </div><!-- /.col-lg-4 -->
+      <div class="col-lg-4">
+        <img class="bd-placeholder-img rounded-circle" src="img/1.jpg" alt="" width="140" height="140">
+       
+        <h2>Heading</h2>
+        <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+        <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
+      </div><!-- /.col-lg-4 -->
+    </div><!-- /.row -->
+
+
+    
+
+  </div><!-- /.container -->
+
+ <!-- Facebook -->
+ <div class=" container marketing">
+  <div class="row">
+    <div class="col-lg-7"  align="left" >
+      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d253151.7847927814!2d-79.08109814131495!3d-7.52068117830804!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xcbc7c26e0b70a809!2sMunicipalidad%20Distrital%20de%20San%20Benito!5e0!3m2!1ses!2spe!4v1612588124752!5m2!1ses!2spe" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+
+    </div>
+    <div class="col-lg-5" align="right">
+      <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fpg%2FMunicipalidad-Distrital-de-San-Benito-Gesti%25C3%25B3n-2019-2022-355365718580908%2Fposts%2F%3Fref%3Dpage_internal&tabs=timeline&width=340&height=500&small_header=true&adapt_container_width=true&hide_cover=false&show_facepile=true&appId" width="600" height="450" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
+    
+    </div>
+  </div>
+
+ <div>
+ 
+</main>
+  <!-- FOOTER -->
+  
+<!---FOOTER-->  
+  <footer class="footer">
+    <div class="container">
+      <div class="row">
+        <div class="footer-col">
+          <h4>Bienvenidos</h4>
+          <ul>
+            <li><a href="#"> 
+              La municipalidad distrital de San Benito te da la bienvenida a través de este 
+              espacio digital, un canal de comunicación de carácter abierto en el que encontráras
+              información del trabajo que venimos realizando para hacer de nuestra ciudad un lugar
+              más seguro, ordenado, limpio, inclusivo y participativo.</a></li>
+          </ul>
+        </div>
+        <div class="footer-col">
+          <h4>Interes</h4>
+          <ul> 
+            <li><a href="#">Organigrama</a></li>
+            <li><a href="#">Concejo Municipal</a></li>
+            <li><a href="#">Regidores</a></li>
+            <li><a href="#">Funcionarios</a></li>
+            <li><a href="#">Personal</a></li>
+            <li><a href="#">Obras Públicas</a></li>
+          </ul>
+        </div>
+        <div class="footer-col">
+          <h4>Emergencias</h4>
+          <ul>          
+            <li><a href="#"><img src="img/policia.png">PNP</a></li>
+            <li><a href="#">xxxx</a></li>
+          </ul>
+        </div>
+        <div class="footer-col">
+          <h4>Contactanos</h4>
+          <ul>
+            <li><a href="#"> Dirección:<br> Jr. Unión N° 206 - San Benito, Cajamarca</a></li>
+            <li><a href="#"> Teléfono:<br> 076-837141.</a></li>
+            <li><a href="#"> Coreeo:<br> mdsb2015@gmail.com</a></li>
+            <li><a href="#"> Atención:<br> lunes a viernes: de 08:00 a 12:30 | 02:00 a 05:30</a></li>
+          </ul>
+          <div class="social-links">
+            <a href="#"><i class="fab fa-facebook-f"></i></a>
+            <a href="#"><i class="fab fa-whatsapp"></i></a>
+            <a href="#"><i class="fab fa-instagram"></i></a>
+          </div>
+        </div>
+      </div>
+    </div>
+      <br>
+      <h4 class="text-light" align="center">Copyright © 2021 Municipalidad Distrital de San Benito</h4>
+
+    </div>
+
+
+    <script src="js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+
+      
+  </body>
+</html>
---
+++ principal.php
@@ -0,0 +1,175 @@
+<!DOCTYPE html>
+
+<html lang="en" dir="ltr">
+   <head>
+      <meta charset="utf-8">
+    
+      <script src="https://code.jquery.com/jquery-3.5.0.js"></script>
+      <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <link href="css/estilos.css" rel="stylesheet" crossorigin="anonymous">
+      <link href="css/es.css" rel="stylesheet" crossorigin="anonymous">
+      <link href="css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
+   </head>
+   <body>
+<style>
+  
+</style>
+<!-- NavBar-->
+
+      <nav>
+         <div class="logo">
+            CodingNepal
+         </div>
+         <label for="btn" class="icon">
+         <span class="fa fa-bars"></span>
+         </label>
+         <input type="checkbox" id="btn">
+         <ul>
+            <li><a href="#">Inicio</a></li>
+            <li>
+               <label for="btn-1" class="show">Nosotros</label>
+               <a href="#">Nosotros</a>
+               <input type="checkbox" id="btn-1">
+               <ul>
+                  <li><a href="#">Alcalde</a></li>
+                  <li><a href="#">Regidores</a></li>
+               </ul>
+            </li>
+            <li>
+               <label for="btn-2" class="show">Oragnizacion</label>
+               <a href="#">Oragnizacion</a>
+               <input type="checkbox" id="btn-2">
+               <ul>
+                  <li><a href="#">Misión</a></li>
+                  <li><a href="#">Visión</a></li>
+               </ul>
+            </li>
+            <li><a href="#">Portfolio</a></li>
+            <li><a href="#">Contactenos</a></li>
+         </ul>
+      </nav>
+    <!--fin NavBar-->
+
+    <!--Carrusel-->
+  <main>
+
+    <div id="myCarousel" class="carousel slide" data-bs-ride="carousel">
+      <ol class="carousel-indicators">
+        <li data-bs-target="#myCarousel" data-bs-slide-to="0" class="active"></li>
+        <li data-bs-target="#myCarousel" data-bs-slide-to="1"></li>
+        <li data-bs-target="#myCarousel" data-bs-slide-to="2"></li>
+      </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="bd-placeholder-img" src="img/8.jpg" alt="" width="100%" height="100%">
+      <div class="container">
+        <div class="carousel-caption text-start bg-dark" style="background-color:transparent; opacity:.6;">
+          <h1>Plaza de Armas San Benito</h1>
+          <p>La Municipalidad Distrital de San Benito te da la Bienvenida</p>
+        </div>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img class="bd-placeholder-img" src="img/1.jpg" alt="" width="100%" height="100%">
+      <div class="container">
+        <div class="carousel-caption text-start bg-dark" style="background-color:transparent; opacity:.6;">
+          <h1>Plaza de Armas San Benito</h1>
+          <p>La Municipalidad Distrital de San Benito te da la Bienvenida</p>
+        </div>
+      </div>
+    </div>
+    <div class="carousel-item">
+      
+      <img class="bd-placeholder-img" src="img/3.jpg" alt="" width="100%" height="100%">
+      <div class="container">
+        <div class="carousel-caption text-start bg-dark" style="background-color:transparent; opacity:.6;">
+          <h1>Plaza de Armas San Benito</h1>
+          <p>La Municipalidad Distrital de San Benito te da la Bienvenida</p>
+         </div>
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#myCarousel" role="button" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#myCarousel" role="button" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </a>
+</div>
+<!--fin Carrusel -->
+
+  <!--imagenes -->
+<div class="container marketing">
+
+
+  <div class="row">
+    <div class="col-lg-4" >
+      <img class="bd-placeholder-img rounded-circle" src="img/1.jpg" alt="" width="140" height="140">
+     
+      <h2>Heading</h2>
+      <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
+      <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
+    </div><!-- /.col-lg-4 -->
+    <div class="col-lg-4">
+      <img class="bd-placeholder-img rounded-circle" src="img/1.jpg" alt="" width="140" height="140">
+     
+      <h2>Heading</h2>
+      <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.</p>
+      <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
+    </div><!-- /.col-lg-4 -->
+    <div class="col-lg-4">
+      <img class="bd-placeholder-img rounded-circle" src="img/1.jpg" alt="" width="140" height="140">
+     
+      <h2>Heading</h2>
+      <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+      <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
+    </div><!-- /.col-lg-4 -->
+  </div><!-- /.row -->
+
+
+  
+
+</div><!-- /.container -->
+
+
+<!-- Facebook -->
+<div class=" container marketing">
+<div class="row">
+  <div class="col-lg-7"  align="left" >
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d253151.7847927814!2d-79.08109814131495!3d-7.52068117830804!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xcbc7c26e0b70a809!2sMunicipalidad%20Distrital%20de%20San%20Benito!5e0!3m2!1ses!2spe!4v1612588124752!5m2!1ses!2spe" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+
+  </div>
+  <div class="col-lg-5" align="right">
+    <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fpg%2FMunicipalidad-Distrital-de-San-Benito-Gesti%25C3%25B3n-2019-2022-355365718580908%2Fposts%2F%3Fref%3Dpage_internal&tabs=timeline&width=340&height=500&small_header=true&adapt_container_width=true&hide_cover=false&show_facepile=true&appId" width="600" height="450" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
+  
+  </div>
+</div>
+
+<div>
+</div>
+</main>
+<!-- FOOTER -->
+<footer class="container">
+  <p class="float-end"><a href="#">Back to top</a></p>
+  <p>&copy; Municipalidad Distrital de San Benito 2021 &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
+</footer>
+
+
+
+
+
+
+  <script src="js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+
+
+
+
+      <script>
+         $('.icon').click(function(){
+           $('span').toggleClass("cancel");
+         });
+      </script>
+   </body>
+</html>
